e4dbe289b02c9529ce1cc3fd62b101aa956b48bd
Artyom M <artem.melnikov@live.com>
2024-06-07 23:05:50 +0300

    Fix usings

--- src/MudBlazor/Utilities/MudColor.cs
+++ src/MudBlazor/Utilities/MudColor.cs
@@ -7,7 +7,6 @@ using System.Globalization;
 using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
 using System.Xml;
-using System.Xml.Linq;
 using System.Xml.Schema;
 using System.Xml.Serialization;
 using MudBlazor.Extensions;
